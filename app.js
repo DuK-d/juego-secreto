08720f58013554fc852798d79d0ee96c7c2b9250
Sebastian <105533082+DuK-d@users.noreply.github.com>
2024-02-09 07:56:26 -0300

    Update app.js

---
+++ app.js
@@ -0,0 +1,40 @@
+//Variables:
+let numeroMaximoPosible =100;
+let numeroSecreto = Math.floor(Math.random()*numeroMaximoPosible)+1;
+let numeroUsuario = 0;
+let intentos = 1;
+//let palabraVeces = 'vez';
+let maximoIntentos = 6;
+
+while(numeroUsuario != numeroSecreto) {
+ numeroUsuario = parseInt(prompt("Me indicas un número entre 1 y ${nuneroMaximoPosible} por favor:"));
+
+console.log(typeof(numeroUsuario));
+/* ESTE CODIGO REALIZA 
+LA COMPARACION...
+*/
+if (numeroUsuario == numeroSecreto) {
+    alert(`Acertaste!!!, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces'}`);
+}    
+else {
+    if(numeroUsuario > numeroSecreto){
+        alert('El número secreto es menor');
+    } else {
+        alert('El número secreto es mayor');
+    }
+    //Incrementamos el contador cuando NO acierte...
+    //intentos = intentos + 1;
+    //intentos += 1;
+    intentos++;
+
+    //palabraVeces = 'veces';
+    if(intentos > maximoIntentos){
+        alert('Llegaste al número máximo de intentos');
+        break;
+    }
+
+//La condicion NO se cumplió
+//alert('lo siento..sigue intentando...');
+
+    }
+}   
